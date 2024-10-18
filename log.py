ac980acca7e13aa74935245a136ffc23c2a7de44
kuangjux <18630816527@163.com>
2024-10-18 09:49:26 +0800

    fix.

--- log.py
+++ log.py
@@ -1,7 +1,12 @@
 import datetime
+
+
 def log(user):
+    today = datetime.date.today()
+    msg = "User " + user + " unfollowed you."
+    msg += "    ------ " + str(today) + '\n'
+
     with open('./log.txt', 'a+') as f:
-        today = datetime.date.today()
-        msg = "User " + user + " unfollowed you."
-        msg += "    ------ " + str(today) + '\n'
         f.write(msg)
+
+    print(msg)
